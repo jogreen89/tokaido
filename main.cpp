32343f9745af6bfd0c130672456bd1bfd46c0569
zubernetes <zubernetes@gmail.com>
2016-03-20 13:21:23 -0400

    First commit of the day. Picking up where I left off.

--- main.cpp
+++ main.cpp
@@ -5,12 +5,12 @@
 #include "bst.h"
 
 int main(int argc, char **argv) {
-    Node* n = new Node(
+    Node m = {.key = 4, .left = NULL, .right = NULL, .parent = NULL };
 
     BST* b = new BST();
-    b->insertNode(&n);
+    b->insertNode(&m);
     
-    printf("here is my root key : %d\n", b->getRoot()->key);
+    printf("here is my n key : %d\n", b->getRoot()->key);
     
 	return 0;
 }
