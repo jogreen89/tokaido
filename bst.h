79eb3e494b946ca48f7bedb55fdd2f2c05d97a4e
zubernetes <zubernetes@gmail.com>
2016-03-21 10:50:44 -0400

    First commit of the day. Adding unsigned int for this implementation.

--- bst.h
+++ bst.h
@@ -18,15 +18,15 @@ struct Node {
 class BST {
 public:
     BST();
-	// ~BST();				  // destructor deletes all nodes 
+	// ~BST();				        // destructor deletes all nodes 
 
     // Helper methods
     Node* getRoot();
     Node* getLeftChild(Node*);
     Node* getRightChild(Node*);
     Node* getParent(Node*);
-    int   getKey(Node*);
-    int   getSize();
+    unsigned int getKey(Node*);
+    unsigned int getSize();
 
     // Helper methods (cont.)
 
@@ -37,13 +37,13 @@ public:
     // Required methods (class)
 
 	void insertNode(Node *n);      // inserts a node n into the tree
-	int deleteNode(int z); 	       // deletes the first node with a value x
-	int successor(int x);          // returns the successor to the node x
+	unsigned int deleteNode(int z);// deletes the first node with a value x
+	unsigned int successor(int x); // returns the successor to the node x
 	void traverseinorder(Node*);   // prints the nodes in the tree in order
-	int findmax(); 		           // returns the manimum value node in the tree
+	unsigned int findmax(Node*);   // returns the manimum value node in the tree
 private:
-	Node *root;	// pointer to first node
-	int size;	// number of nodes in the tree
+	Node *root;	                   // pointer to first node
+	int size;	                   // number of nodes in the tree
 };
 
 BST::BST() {
@@ -78,11 +78,11 @@ Node* BST::getParent(Node *n) {
     return n->parent;
 }
 
-int BST::getKey(Node *n) {
+unsigned int BST::getKey(Node *n) {
     return n->key;
 }
 
-int BST::getSize() {
+unsigned int BST::getSize() {
     return size;
 }
 
@@ -111,5 +111,10 @@ void BST::traverseinorder(Node *n) {
         traverseinorder(n->right);
     }
 }
-	
+
+unsigned int BST::findmax(Node *n) {
+    printf("findmax\n");
+    return 5;
+}
+
 #endif 
