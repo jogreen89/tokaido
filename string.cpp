0522d191be5c16d73ae45354823c800271060e59
foucodian <foucodian@gmail.com>
2016-03-22 19:50:48 -0400

    Rewrites done for now. Continuing to build bst.h

--- string.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-
-int main(int argc, char **argv) {
-	std::string name;
-	std::cout << "Please enter you name: \n";
-	std::cin >> name;
-	std::cout << "Your name is " << name << "\n";
-}
