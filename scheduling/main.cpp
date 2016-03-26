80405faa5ac962dd5a7cbe0156e5ed6945ae336d
foucodian <foucodian@gmail.com>
2016-03-26 13:25:06 -0400

    First commit of the day. Sketching scheduling simulation.

---
+++ scheduling/main.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+#include "fcfs.h"
+#include "rr.h"
+#include "sjf.h"
+
+// Task Format:
+// pid arrival_time burst_time
+
+int main(int argc, char **argv) {
+   printf("Hello, World\n"); 
+}
---
+++ scheduling/rr.h
@@ -0,0 +1,19 @@
+#ifndef RR_H
+#define RR_H
+
+void printrr();
+
+void printrr() {
+/* 
+    The selected scheduling algorithms to implement
+    in this project are:
+
+    1) First Come First Serve (FCFS)
+    2) Round Robin
+    3) Shortest Job First (SJF)
+
+    The details of these algorithms are available in the text book.
+*/
+}
+
+#endif
