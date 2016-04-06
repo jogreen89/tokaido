bce783e42e54077aa866885da4ddc5d339874cc3
zubernetes <zubernetes@gmail.com>
2016-04-05 22:12:49 -0400

    Bug fixed. Huge relief, might be getting out of my funk.

--- scheduling/rr.h
+++ scheduling/rr.h
@@ -4,51 +4,47 @@
 int operateRR(std::vector<PCB*>, int);
 
 int operateRR(std::vector<PCB*> v, int q) {
+
     std::vector<PCB*>::iterator it;
     int process       = 0,
         arrivalTime   = 0,
-        burstTime     = 0,
+        burstSize     = 0,
         systemTime    = 0,
         wait_time     = 0,
         quantum       = q,
         processNum    = 0,
-        difference    = 0,
-        containerSize = v.size();
+        difference    = 0;
 
     while(!v.empty()) {
-        // calculate average time of execution
-        struct timeval wait_start, wait_stop;
-        wait_start.tv_usec = 0, wait_stop.tv_usec = 0;
-        gettimeofday(&wait_start, NULL);
+        struct timeval wait_start, wait_stop;           // 
+        wait_start.tv_usec = 0, wait_stop.tv_usec = 0;  // Calculate average time of execution.
+        gettimeofday(&wait_start, NULL);                //
 
         process      = v[processNum]->getProcessID();
         arrivalTime  = v[processNum]->getArrivalTime();
-        burstTime    = v[processNum]->getBurstSize();
+        burstSize    = v[processNum]->getBurstSize();
 
         for (int i = 0; i < quantum; i++) {
             std::cout << "<system time " << systemTime
                       << "> process "    << process
-                      << " is runnning " << " : "
-                      << burstTime       << std::endl;
+                      << " is running "  << std::endl;
             systemTime++;
         }
 
-        difference = burstTime - q;
+        difference = burstSize - q;
         v[processNum]->setBurstSize(difference);
+
+        if (burstSize <= 0) {
+            v.erase(v.begin()+processNum);
+        }
+
+        processNum++;
+        if (processNum >= v.size() - 1)
+            processNum = 0;
 
         gettimeofday(&wait_stop, NULL);
         wait_time = ((wait_stop.tv_sec - wait_start.tv_sec) * 1000000 +
             wait_stop.tv_usec - wait_start.tv_usec);
-
-        processNum++;
-        
-
-        if (v[processNum]->getBurstSize() <= 0) {
-            v.erase(v.begin()+processNum);
-        }
-
-        if (processNum >= containerSize-1)
-            processNum = 0;
     }
     std::cout << "<system time " << systemTime << "> All processes " <<
        "finish...................." << std::endl;
