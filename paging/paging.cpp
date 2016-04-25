1bebbb9bec7b87df15b27465c331edbfe58cbc54
zubernetes <zubernetes@gmail.com>
2016-04-25 10:33:47 -0400

    Added control simulation control blocks in main()

--- paging/paging.cpp
+++ paging/paging.cpp
@@ -1,4 +1,12 @@
+// paging.cpp
+//
+// A simple C++ simulation of 
+// of page replacement algorithms commonly
+// used in operating systems.
+// 2016 (c) zubernetes 
 #include <iostream>
+
+#include "random.h"
 
 #define ARGS 2
 void exec_fifo(void);
@@ -8,14 +16,24 @@ void exec_optimal(void);
 int main(int argc, char **argv) {
     if (argc < ARGS) 
         std::cout << "Usage: paging [fifo|lru|optimal]" << std::endl;
-    else
-        std::cout << "Hello, World!" << std::endl;
+    if (argv[1] != NULL) {
+        if (strncmp(argv[1], "FIFO", 2) == 0 ||
+            strncmp(argv[1], "fifo", 2) == 0) 
+            exec_fifo();
+        if (strncmp(argv[1], "LRU", 2) == 0 ||
+            strncmp(argv[1], "lru", 2) == 0) 
+            exec_lru();
+        if (strncmp(argv[1], "OPTIMAL", 2) == 0 ||
+            strncmp(argv[1], "optimal", 2) == 0) 
+            exec_optimal();
+    }
     return 0;
 }
 
 void exec_fifo(void) {
     std::cout << "Implementation of FIFO page replacement algorithm."
         << std::endl;
+    generateString();
 }
 
 void exec_lru(void) {
---
+++ paging/random.h
@@ -0,0 +1,9 @@
+#ifndef RANDOM_H
+#define RANDOM_H
+
+void generateString(void) {
+    std::cout << "I am generating a string here."
+        << std::endl;
+}
+
+#endif 
